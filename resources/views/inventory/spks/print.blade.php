e2c1ec6b8b992eff838cf459e8651a51e66c1fb2
Jazmanudin27 <jazmanudin9@gmail.com>
2026-07-28 12:03:50 +0700

    update

--- resources/views/inventory/spks/print.blade.php
+++ resources/views/inventory/spks/print.blade.php
@@ -185,7 +185,7 @@
         .design-img {
             width: 100%;
             height: 100%;
-            max-height: 100%;
+            max-height: 350px;
             object-fit: contain;
         }
 
