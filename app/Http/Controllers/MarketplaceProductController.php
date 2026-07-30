1aa9e9fbf746828b82a55bedd5ff00ec04c0f73a
Jazmanudin27 <jazmanudin9@gmail.com>
2026-07-30 15:33:48 +0700

    Allow searching linked MasterProduct SKU in Marketplace Product table filter

--- app/Http/Controllers/MarketplaceProductController.php
+++ app/Http/Controllers/MarketplaceProductController.php
@@ -34,7 +34,13 @@ class MarketplaceProductController extends Controller
         }
 
         if ($request->filled('sku')) {
-            $query->where('marketplace_products.marketplace_sku', 'like', '%' . $request->sku . '%');
+            $skuInput = trim($request->sku);
+            $query->where(function ($q) use ($skuInput) {
+                $q->where('marketplace_products.marketplace_sku', 'like', '%' . $skuInput . '%')
+                  ->orWhereHas('masterProduct', function ($mq) use ($skuInput) {
+                      $mq->where('sku', 'like', '%' . $skuInput . '%');
+                  });
+            });
         }
 
         if ($request->filled('channel_id')) {
